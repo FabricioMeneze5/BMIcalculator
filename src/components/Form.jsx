f0a6161a081ff97dac06bb10bae44b2e45f80e16
FabricioMeneze5 <fabricio-menezes@live.com>
2024-07-21 18:02:22 +0100

    ADD info with colors

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -3,53 +3,49 @@ import { useState } from "react";
 const Form = () => {
     const [weight, setWeight] = useState("");
     const [height, setHeight] = useState("");
-    const [result, setResult] = useState("");
     const [showBMI, setShowBMI] = useState("Calculate your BMI and check the result with the table below.");
-    const [classSpan, setClassSpan] = useState("");
+    const [classSpan, setClassSpan] = useState("light");
 
     const calculateBMI = () => {
         if (weight < 2 || weight > 350) {
-            alert('Please, fill a valid weight');
             setHeight("");
             setWeight("");
-            setResult("");
-            setClassSpan("");
+            setShowBMI("Please, fill a valid weight!");
+            setClassSpan("light");
         } else if (height < 0.5 || height > 2.0) {
-            alert('Please, fill a valid height');
             setHeight("");
             setWeight("");
-            setResult("");
-            setClassSpan("");
+            setShowBMI("Please, fill a valid height!");
+            setClassSpan("light");
         }
 
         const BMI = weight / (height * height);
         const resultFormated = BMI.toFixed(2);
-        setResult(resultFormated);
 
         if (BMI <= 17.0) {
-            setShowBMI("Insufficient Weight");
-            setClassSpan("text-bg-info");
+            setShowBMI(`Your BMI is ${resultFormated} - Insufficient Weight`);
+            setClassSpan("info");
         } else if (BMI <= 24.9) {
-            setShowBMI("Normal weight");
-            setClassSpan("text-bg-success");
+            setShowBMI(`Your BMI is ${resultFormated} - Normal weight`);
+            setClassSpan("success");
         } else if (BMI <= 26.9) {
-            setShowBMI("Overweight grade I");
-            setClassSpan("text-bg-warning");
+            setShowBMI(`Your BMI is ${resultFormated} - Overweight grade I`);
+            setClassSpan("secondary");
         } else if (BMI <= 29.9) {
-            setShowBMI("Overweight grade II");
-            setClassSpan("text-bg-warning");
+            setShowBMI(`Your BMI is ${resultFormated} - Overweight grade II`);
+            setClassSpan("secondary");
         } else if (BMI <= 34.9) {
-            setShowBMI("Type I obesity");
-            setClassSpan("text-bg-danger");
+            setShowBMI(`Your BMI is ${resultFormated} - Type I obesity`);
+            setClassSpan("warning");
         } else if (BMI <= 39.9) {
-            setShowBMI("Type II obesity");
-            setClassSpan("text-bg-danger");
+            setShowBMI(`Your BMI is ${resultFormated} - Type II obesity`);
+            setClassSpan("warning");
         } else if (BMI <= 49.9) {
-            setShowBMI("Type III obesity morbid");
-            setClassSpan("text-bg-dark");
+            setShowBMI(`Your BMI is ${resultFormated} - Type III obesity morbid`);
+            setClassSpan("danger");
         } else if (BMI >= 50.0) {
-            setShowBMI("Type III obesity morbid");
-            setClassSpan("text-bg-dark");
+            setShowBMI(`Your BMI is ${resultFormated} - Type III obesity morbid`);
+            setClassSpan("danger");
         } else {
             setShowBMI("Calculate your BMI and check the result with the table below.");
         }
@@ -79,9 +75,7 @@ const Form = () => {
 
                 <button type="button" onClick={calculateBMI} className="btn btn-sm btn-outline-primary w-100 mb-3">Calculate</button>
 
-                <span className={`form-control text-center ${classSpan}`}>
-                    {result}
-                    <br />
+                <span className={`badge w-100 text-center text-wrap text-bg-${classSpan}`}>
                     {showBMI}
                 </span>
 
--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,43 +1,43 @@
 
 const Table = () => {
     return (
-        <table className="table table-striped w-50 border border-5 text-center">
+        <table className="table border w-50 border-5 text-center">
             <thead>
                 <tr>
-                    <th>BMI / IMC</th>
+                    <th>BMI</th>
                     <th>Classification</th>
                 </tr>
             </thead>
             <tbody>
-                <tr>
+                <tr className="table-info">
                     <td>Less than 17</td>
                     <td>Insufficient Weight</td>
                 </tr>
-                <tr>
+                <tr className="table-success">
                     <td>18,5 - 24,9</td>
                     <td>Normal weight</td>
-                </tr >
-                <tr>
+                </tr>
+                <tr className="table-secondary">
                     <td>25 - 26,9</td>
                     <td>Overweight grade I</td>
                 </tr>
-                <tr>
+                <tr className="table-secondary">
                     <td>27 - 29,9</td>
                     <td>Overweight grade II</td>
                 </tr>
-                <tr>
+                <tr className="table-warning">
                     <td>30 - 34,9</td>
                     <td>Type I obesity</td>
                 </tr>
-                <tr>
+                <tr className="table-warning">
                     <td>35 - 39,9</td>
                     <td>Type II obesity</td>
                 </tr>
-                <tr>
+                <tr className="table-danger">
                     <td>40 - 49,9</td>
                     <td>Type III obesity morbid</td>
                 </tr>
-                <tr>
+                <tr className="table-danger">
                     <td>more than 50</td>
                     <td>Type VI obesity extreme</td>
                 </tr>
